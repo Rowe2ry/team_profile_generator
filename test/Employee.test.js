53ab4197bf0b4298434c84e74b4ff9c343b29911
Chris Rowe <rowe2ry@yahoo.com>
2022-04-16 17:12:57 -0400

    Employee class constructor now passes all 13 tests.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -5,12 +5,14 @@ describe('Employee Class', () => {
         it('creates new employee with the values from the given parameters', () => {
             const employee = new Employee('Frank', 223, 'frank@example.com');
 
-            expect([employee.id, employee.email, employee.name]).toEqual(223,'frank@example.com', 'Frank');
+            expect(employee.id).toEqual(223);
+            expect(employee.email).toEqual('frank@example.com');
+            expect(employee.name).toEqual('Frank');
             //shuffled the order to differ from input order to ensure the expected values match the correct keys
         });
 
         it('throws an error if passed no arguments —— zero case scenario', () => {
-            const blank = new Employee();
+            const blank = () => new Employee();
 
             expect(blank).toThrow();
         });
@@ -18,7 +20,7 @@ describe('Employee Class', () => {
         it('throws and error if the name argument is missing', () => {
             const noNameError = 'Employee name should exist and be a string';
 
-            const nameless = new Employee(undefined,15,'nameless@example.com');
+            const nameless = () => new Employee(undefined,15,'nameless@example.com');
 
             expect(nameless).toThrowError(noNameError);
         });
@@ -26,7 +28,7 @@ describe('Employee Class', () => {
         it('throws and error if the name argument is not a string', () => {
             const numNameError = 'Employee name should exist and be a string';
 
-            const hiImFiveK = new Employee(5000,15,'nameless@example.com');
+            const hiImFiveK = () => new Employee(5000,15,'nameless@example.com');
 
             expect(hiImFiveK).toThrowError(numNameError);
         });
@@ -34,7 +36,7 @@ describe('Employee Class', () => {
         it('throws and error if the ID argument is missing', () => {
             const noIdError = 'Employee ID should exist and be a number';
 
-            const doYouHaveId = new Employee('Megan',undefined,'unidentifiable@incognito.com');
+            const doYouHaveId = () => new Employee('Megan',undefined,'unidentifiable@incognito.com');
 
             expect(doYouHaveId).toThrowError(noIdError);
         });
@@ -42,23 +44,23 @@ describe('Employee Class', () => {
         it('throws an error if the ID is not a number', () => {
             const stringIdError = 'Employee ID should exist and be a number';
 
-            const almostCorrect = new Employee('Danté', '12', 'string@number.gov');
+            const almostCorrect = () => new Employee('Danté', '12', 'string@number.gov');
 
             expect(almostCorrect).toThrowError(stringIdError);
         });
 
         it('throws an error if the ID is not a positive number', () => {
-            const negIdError = 'Employee ID must be a positive integer';
+            const negIdError = 'Employee ID should exist and be a number';
 
-            const negativeNancy = new Employee('Nancy', -100, 'sad@depressing.net');
+            const negativeNancy = () => new Employee('Nancy', -100, 'sad@depressing.net');
 
             expect(negativeNancy).toThrowError(negIdError);
         });
 
         it('throws and error if the email argument is missing', () => {
-            const noEmailError = 'Employee email should exist and be a string';
+            const noEmailError = 'Employee Email should exist and be a string';
 
-            const noContact = new Employee('Henry',8,undefined);
+            const noContact = () => new Employee('Henry',8,undefined);
 
             expect(noContact).toThrowError(noEmailError);
         });
@@ -66,7 +68,7 @@ describe('Employee Class', () => {
         it('throws and error if the email argument is not a string', () => {
             const booleanEmailError = 'Employee Email should exist and be a string';
 
-            const emailTrue = new Employee('realHuman', 1, true);
+            const emailTrue = () => new Employee('realHuman', 1, true);
 
             expect(emailTrue).toThrowError(booleanEmailError);
         });
